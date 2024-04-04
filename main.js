082c6fa31fbaa949f4a88a3b6d022c37f2d57e7c
Pikrpan <54728861+Pikrpan@users.noreply.github.com>
2024-04-04 23:08:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//Vytvoř proměnnou, která bude sloužit pro iteraci
+
+//Vytvoř proměnnou tak, aby se odkazovala na HTML element h2
+
+//Změň text elementu h2 tak, aby ukazoval, kolikrát si kliknul/a na tlačítko
+
+
+function prictiJedna(){
+    //Zvyš hodnotu proměnné určené k iteraci o jedna
+
+    //Změň text elementu h2 tak, aby ukazoval, kolikrát si kliknul/a na tlačítko
+
+    /*Napiš alespoň 3 podmínky, které změní barvu textu elementu h2 podle toho,
+      kolikrát si na něj kliknul/a (10 kliknutí = modrá, 20 = červená, ...)*/
+    
+}
